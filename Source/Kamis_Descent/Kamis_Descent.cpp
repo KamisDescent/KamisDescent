f33225471a644e47313efd8b3dadf6534ccb7bb5
IM56 <imovahedi56@gmail.com>
2019-02-26 14:33:01 +0000

    Resyncing files

---
+++ Source/Kamis_Descent/Kamis_Descent.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Kamis_Descent.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Kamis_Descent, "Kamis_Descent" );
